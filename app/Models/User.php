643aa2b6644f0712230200c03e8e57c971ddad3b
Neil Francis Britos <neilexfrancis18@gmail.com>
2026-07-11 16:13:57 +0800

    Models -> pk_uuid

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,12 +7,11 @@ use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 #[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
-class User extends Authenticatable
+class User extends AuthenticatableModel
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
